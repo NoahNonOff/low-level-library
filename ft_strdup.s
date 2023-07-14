30490751f76140d513e49efa7678557c22f5ec8d
Noah <121849234+NoahNonOff@users.noreply.github.com>
2023-07-14 22:21:50 +0200

    Update ft_strdup.s

--- ft_strdup.s
+++ ft_strdup.s
@@ -10,6 +10,27 @@ global	ft_strdup
 extern	malloc, ft_strlen, ft_strcpy
 
 ft_strdup:
+	cmp rdi, NULL ; check if str == NULL
+	jz exit_error
+	call ft_strlen ; rax = ft_strlen
+	inc rax ; rax++
+	push rdi ; save rdi in stack
+	mov rdi, rax
+	call malloc WRT ..plt; call malloc
+	;"WRT" means "With Respect To ...", i.e, "in the context of ..."
+
+	test rax, rax ; if malloc failed
+	jz exit_error
+
+	pop r8
+	mov rdi, rax
+	mov rsi, r8
+	call ft_strcpy
+	ret
+
+exit_error:
+	mov rax, NULL ; return NULL
+	ret
 
 
 ; arguments:
